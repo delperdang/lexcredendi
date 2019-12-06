1e8a5df48cbba5fb4c93da82ad3c8b149cd7e077
dieselpwr <dieselpwr99@gmail.com>
2019-12-06 00:55:14 -0600

    still working on superuser command 3

--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
+import os
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
+    if not User.objects.filter(username='dieselpwr').exists():
+        User.objects.create_superuser('dieselpwr', 'dieselpwr99@gmail.com', os.environ['SUPERUSER_PASS'])
     return render(request, 'home/landing.html')
