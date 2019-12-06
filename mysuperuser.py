86cc746fd28020cc0122e0240dc033a5ca5309cd
dieselpwr <dieselpwr99@gmail.com>
2019-12-06 00:50:04 -0600

    still working on superuser command 2

--- mysuperuser.py
+++ mysuperuser.py
@@ -1,5 +1,5 @@
 import os
-from django.contrib.auth.models import User
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lexcredendi.settings')
+from django.contrib.auth.models import User
 if not User.objects.filter(username='dieselpwr').exists():
     User.objects.create_superuser('dieselpwr', 'dieselpwr99@gmail.com', os.environ['SUPERUSER_PASS'])
