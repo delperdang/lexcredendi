82fd1b09d2c66bdd6c33f3307e0b65e762f6b550
dieselpwr <dieselpwr99@gmail.com>
2019-12-07 23:06:41 -0600

    added separate url for creating my super user

--- home/views.py
+++ home/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
+    return render(request, 'home/landing.html')
+
+def mysuperuser(request):
     if not User.objects.filter(username='dieselpwr').exists():
         User.objects.create_superuser('dieselpwr', 'dieselpwr99@gmail.com', os.environ['SUPERUSER_PASS'])
     return render(request, 'home/landing.html')
