40e5a19b8fbad87c440fe3c7cc052402f64519cd
dieselpwr <dieselpwr99@gmail.com>
2019-12-11 12:29:29 -0600

    simplified data models by removing comments

--- doctrine/models.py
+++ doctrine/models.py
@@ -7,7 +7,6 @@ class Record(models.Model):
     code = models.CharField(_('Code'), max_length=255, primary_key=True)
     title = models.CharField(_('Title'), max_length=255)
     text = models.CharField(_('Text'), max_length=4000)
-    comment = models.CharField(_('Comment'), max_length=255, blank=True, null=True)
     def __str__(self):
         return ('{}').format(self.title)
     class Meta:
--- prayer/models.py
+++ prayer/models.py
@@ -5,13 +5,11 @@ from django.utils.translation import gettext as _
 
 class Record(models.Model):
     code = models.CharField(_('Code'), max_length=255, primary_key=True)
-    english_title = models.CharField(_('English Title'), max_length=255)
-    english_text = models.CharField(_('English Text'), max_length=4000)
-    english_comment = models.CharField(_('English Comment'), max_length=255, blank=True, null=True)
+    title = models.CharField(_('English Title'), max_length=255)
+    text = models.CharField(_('English Text'), max_length=4000)
     latin_title = models.CharField(_('Latin Title'), max_length=255, blank=True, null=True)
     latin_text = models.CharField(_('Latin Text'), max_length=4000, blank=True, null=True)
-    latin_comment = models.CharField(_('Latin omment'), max_length=255, blank=True, null=True)
     def __str__(self):
-        return ('{}').format(self.english_title)
+        return ('{}').format(self.title)
     class Meta:
         verbose_name_plural = "Records"
