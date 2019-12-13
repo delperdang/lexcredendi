f24c5cfafc79f9f1cfd2b32ede11a66a62b93c41
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 00:31:30 -0600

    added art app

---
+++ art/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+# Create your models here.
+
+class Record(models.Model):
+    code = models.CharField(_('Code'), max_length=255, primary_key=True)
+    title = models.CharField(_('Title'), max_length=255)
+    album = models.CharField(_('Collection'), max_length=255)
+    work = models.ImageField(_('Artwork'), upload_to='art/')
+    artist = models.CharField(_('Artist'), max_length=255, blank=True, null=True)
+    rough_date = models.CharField(_('Date'), max_length=255, blank=True, null=True)
+    def __str__(self):
+        return ('{}').format(self.title)
+    class Meta:
+        verbose_name_plural = "Records"
