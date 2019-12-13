1181c7580b1d7af3a4ab3aee019b148ca1adf5db
dieselpwr <dieselpwr99@gmail.com>
2019-12-12 23:35:16 -0600

    added localtime check

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from home import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('createsuperuser', views.mysuperuser, name='mysuperuser'),
+    path('localtime', views.localtime, name='localtime'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 import os
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.utils import timezone
 
 # Create your views here.
 
@@ -11,3 +13,8 @@ def mysuperuser(request):
     if not User.objects.filter(username='dieselpwr').exists():
         User.objects.create_superuser('dieselpwr', 'dieselpwr99@gmail.com', os.environ['SUPERUSER_PASS'])
     return render(request, 'home/landing.html')
+
+def localtime(request):
+    now = timezone.localtime()
+    html = str(now)
+    return HttpResponse(html)
