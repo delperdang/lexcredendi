fcbf2d454f19acf8ce212f678f4cb4c820ffa989
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 00:34:36 -0600

    added art app extras (not installed)

--- art/views.py
+++ art/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from art.models import Record
 
 # Create your views here.
+
+def home(request):
+    records = Record.objects.all().order_by('code')
+
+    context = {
+        'records': records
+    }
+
+    return render(request, 'art/home.html', context)
