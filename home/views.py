971ce4fbccc5c5ff96445c130152165bb0cc24f1
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 14:27:28 -0600

    simplified mysuperuser check

--- home/views.py
+++ home/views.py
@@ -12,7 +12,10 @@ def home(request):
 def mysuperuser(request):
     if not User.objects.filter(username='dieselpwr').exists():
         User.objects.create_superuser('dieselpwr', 'dieselpwr99@gmail.com', os.environ['SUPERUSER_PASS'])
-    return render(request, 'home/landing.html')
+        html = 'created'
+    else:
+        html = 'exists'
+    return HttpResponse(html)
 
 def localtime(request):
     now = timezone.localtime()
