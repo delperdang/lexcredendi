937c53e3c91477d6b7c10cdb30443ab51e23050e
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 17:42:43 -0600

    updated art details template

--- art/models.py
+++ art/models.py
@@ -7,7 +7,7 @@ class Record(models.Model):
     code = models.CharField(_('Code'), max_length=255, primary_key=True)
     title = models.CharField(_('Title'), max_length=255)
     album = models.CharField(_('Collection'), max_length=255)
-    work = models.ImageField(_('Artwork'), upload_to='art/')
+    img = models.ImageField(_('Artwork'), default="img.jpg")
     artist = models.CharField(_('Artist'), max_length=255, blank=True, null=True)
     rough_date = models.CharField(_('Date'), max_length=255, blank=True, null=True)
     def __str__(self):
