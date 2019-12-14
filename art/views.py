1b134a3a3b6b5e7c6eb151bf3d3db5d9ff639d1d
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 23:02:11 -0600

    removed bad migrations

--- art/views.py
+++ art/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
-from art.models import Record
+# from art.models import Record
 
 # Create your views here.
 
 def home(request):
-    records = Record.objects.all().order_by('code')
+    # records = Record.objects.all().order_by('code')
 
     return render(request, 'art/home.html')
 
 def details(request, album):
-    records = Record.objects.filter(album=album)
+    # records = Record.objects.filter(album=album)
 
     context = {
-        'records': records
+        # 'records': records
     }
 
     return render(request, 'art/details.html', context)
