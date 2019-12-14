e4d4da30ef823e895c743a82e1d62eb80bbad1d3
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 23:00:44 -0600

    removed bad migrations

--- art/models.py
+++ art/models.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext as _
 
 # Create your models here.
 
-class Record(models.Model):
-    filename = models.CharField(_('Filename'), max_length=255, primary_key=True, default='test.jpg')
-    title = models.CharField(_('Title'), max_length=255)
-    album = models.CharField(_('Album'), max_length=255)
-    artist = models.CharField(_('Artist'), max_length=255, blank=True, null=True)
-    rough_date = models.CharField(_('Rough Date'), max_length=255, blank=True, null=True)
-    def __str__(self):
-        return ('{}').format(self.title)
-    class Meta:
-        verbose_name_plural = "Records"
+# class Record(models.Model):
+#     filename = models.CharField(_('Filename'), max_length=255, primary_key=True, default='test.jpg')
+#     title = models.CharField(_('Title'), max_length=255)
+#     album = models.CharField(_('Album'), max_length=255)
+#     artist = models.CharField(_('Artist'), max_length=255, blank=True, null=True)
+#     rough_date = models.CharField(_('Rough Date'), max_length=255, blank=True, null=True)
+#     def __str__(self):
+#         return ('{}').format(self.title)
+#     class Meta:
+#         verbose_name_plural = "Records"
