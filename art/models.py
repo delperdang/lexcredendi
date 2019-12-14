9e5de65d26c24e7434f12e5de61b4e4607aec8c2
dieselpwr <dieselpwr99@gmail.com>
2019-12-13 23:23:13 -0600

    new migrations

--- art/models.py
+++ art/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 # Create your models here.
 
 class Record(models.Model):
-    filename = models.CharField(_('Filename'), max_length=255, primary_key=True, default='test.jpg')
+    filename = models.CharField(_('Filename'), max_length=255, primary_key=True)
     title = models.CharField(_('Title'), max_length=255)
     album = models.CharField(_('Album'), max_length=255)
     def __str__(self):
