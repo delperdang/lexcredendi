efc8e8309f0cd6135b22c8d31e894f306280e61b
dieselpwr <dieselpwr99@gmail.com>
2019-12-14 22:01:05 -0600

    performing a template consolidation for faster re-factoring (mepiety)

--- piety/views.py
+++ piety/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render
 from piety.models import Record
 
 # Create your views here.
+APP_NAME = 'piety'
+APP_FULL_NAME = 'Personal Piety'
+ICON_FILENAME = 'cspb.svg'
 
 def home(request):
     records = Record.objects.all().order_by('code')
 
     context = {
+        'app_name': APP_NAME,
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'records': records
     }
 
-    return render(request, 'piety/home.html', context)
+    return render(request, 'home/home.html', context)
 
 def details(request, code):
     record = Record.objects.get(pk=code)
 
     context = {
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'record': record
     }
 
-    return render(request, 'piety/details.html', context)
+    return render(request, 'home/details.html', context)
