b9e02a994e851499e8fa4580a96a39e2e47bd055
dieselpwr <dieselpwr99@gmail.com>
2019-12-14 21:11:38 -0600

    performing a template consolidation for faster re-factoring (doctrine)

--- doctrine/views.py
+++ doctrine/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render
 from doctrine.models import Record
 
 # Create your views here.
+APP_NAME = 'doctrine'
+APP_FULL_NAME = 'Doctrinal Formulae'
+ICON_FILENAME = 'pope.svg'
 
 def home(request):
     records = Record.objects.all().order_by('code')
 
     context = {
+        'app_name': APP_NAME,
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'records': records
     }
 
-    return render(request, 'doctrine/home.html', context)
+    return render(request, 'home/home.html', context)
 
 def details(request, code):
     record = Record.objects.get(pk=code)
 
     context = {
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'record': record
     }
 
-    return render(request, 'doctrine/details.html', context)
+    return render(request, 'home/details.html', context)
