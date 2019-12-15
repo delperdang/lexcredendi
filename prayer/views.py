6f286f7a9fec0f33c531426df057079dc31631e5
dieselpwr <dieselpwr99@gmail.com>
2019-12-14 22:05:10 -0600

    performing a template consolidation for faster re-factoring (meprayer)

--- prayer/views.py
+++ prayer/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render
 from prayer.models import Record
 
 # Create your views here.
+APP_NAME = 'prayer'
+APP_FULL_NAME = 'Prayer Texts'
+ICON_FILENAME = 'priest.svg'
 
 def home(request):
     records = Record.objects.all().order_by('code')
 
     context = {
+        'app_name': APP_NAME,
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'records': records
     }
 
-    return render(request, 'prayer/home.html', context)
+    return render(request, 'home/home.html', context)
 
 def details(request, code):
     record = Record.objects.get(pk=code)
 
     context = {
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'record': record
     }
 
-    return render(request, 'prayer/details.html', context)
+    return render(request, 'home/details.html', context)
