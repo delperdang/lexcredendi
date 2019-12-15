28c7c45e1c2e1c557daa4ec6729a832a46ac7e71
dieselpwr <dieselpwr99@gmail.com>
2019-12-14 21:58:22 -0600

    performing a template consolidation for faster re-factoring (meditation)

--- meditation/views.py
+++ meditation/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+APP_NAME = 'meditation'
+APP_FULL_NAME = 'Breathing Meditation'
+ICON_FILENAME = 'crucifix.svg'
 
 def home(request):
-    return render(request, 'meditation/home.html')
+
+    context = {
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME
+    }
+
+    return render(request, 'meditation/home.html', context)
