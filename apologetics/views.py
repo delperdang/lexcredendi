abccbe558ee6f7d199724b2b4ccc67e3c76d6372
dieselpwr <dieselpwr99@gmail.com>
2019-12-14 20:44:06 -0600

    performing a template consolidation for faster re-factoring

--- apologetics/views.py
+++ apologetics/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render
 from apologetics.models import Record
 
 # Create your views here.
+APP_NAME = 'apologetics'
+APP_FULL_NAME = 'Apologetics Guide'
+ICON_FILENAME = 'bishop.svg'
 
 def home(request):
     records = Record.objects.all().order_by('code')
 
     context = {
+        'app_name': APP_NAME,
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'records': records
     }
 
-    return render(request, 'apologetics/home.html', context)
+    return render(request, 'home/home.html', context)
 
 def details(request, code):
     record = Record.objects.get(pk=code)
 
     context = {
+        'app_full_name': APP_FULL_NAME,
+        'icon_filename': ICON_FILENAME,
         'record': record
     }
 
-    return render(request, 'apologetics/details.html', context)
+    return render(request, 'home/details.html', context)
