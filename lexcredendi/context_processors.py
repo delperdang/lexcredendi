d240719775b41c2f6cd595ff670830cc0f5d96d5
dieselpwr <dieselpwr99@gmail.com>
2019-12-31 22:37:50 -0600

    switched context processor to localtime

--- lexcredendi/context_processors.py
+++ lexcredendi/context_processors.py
@@ -1,31 +1,32 @@
-from datetime import datetime, date, time
+from django.utils import timezone
+from datetime import date
 from dateutil.easter import easter
 from dateutil.relativedelta import relativedelta as rd
 from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 
-baptism_of_our_lord = date(datetime.now().year, 1, 6) + rd(days=1, weekday=SU(+1))
-ash_wednesday = easter(datetime.now().year) - rd(days=46)
-maundy_thursday = easter(datetime.now().year) - rd(days=3)
-pentecost = easter(datetime.now().year) + rd(days=49)
-first_sunday_of_advent = date(datetime.now().year, 12, 25) - rd(days=1, weekday=SU(-4))
-christmas = date(datetime.now().year, 12, 25)
+baptism_of_our_lord = date(timezone.localtime().year, 1, 6) + rd(days=1, weekday=SU(+1))
+ash_wednesday = easter(timezone.localtime().year) - rd(days=46)
+maundy_thursday = easter(timezone.localtime().year) - rd(days=3)
+pentecost = easter(timezone.localtime().year) + rd(days=49)
+first_sunday_of_advent = date(timezone.localtime().year, 12, 25) - rd(days=1, weekday=SU(-4))
+christmas = date(timezone.localtime().year, 12, 25)
 
 def environ_vars(request):
     data = {}
-    if datetime.now().date() < baptism_of_our_lord:
+    if timezone.localtime().date() < baptism_of_our_lord:
         data['SEASONAL_COLOR'] = 'bg-gold'
-    elif datetime.now().date() >= baptism_of_our_lord and datetime.now().date() < ash_wednesday:
+    elif timezone.localtime().date() >= baptism_of_our_lord and timezone.localtime().date() < ash_wednesday:
         data['SEASONAL_COLOR'] = 'bg-success'
-    elif datetime.now().date() >= ash_wednesday and datetime.now().date() < maundy_thursday:
+    elif timezone.localtime().date() >= ash_wednesday and timezone.localtime().date() < maundy_thursday:
         data['SEASONAL_COLOR'] = 'bg-purple'
-    elif datetime.now().date() >= maundy_thursday and datetime.now().date() < easter(datetime.now().year):
+    elif timezone.localtime().date() >= maundy_thursday and timezone.localtime().date() < easter(timezone.localtime().year):
         data['SEASONAL_COLOR'] = 'bg-danger'
-    elif datetime.now().date() >= easter(datetime.now().year) and datetime.now().date() < pentecost:
+    elif timezone.localtime().date() >= easter(timezone.localtime().year) and timezone.localtime().date() < pentecost:
         data['SEASONAL_COLOR'] = 'bg-gold'
-    elif datetime.now().date() >= pentecost and datetime.now().date() < first_sunday_of_advent:
+    elif timezone.localtime().date() >= pentecost and timezone.localtime().date() < first_sunday_of_advent:
         data['SEASONAL_COLOR'] = 'bg-success'
-    elif datetime.now().date() >= first_sunday_of_advent and datetime.now().date() < christmas:
+    elif timezone.localtime().date() >= first_sunday_of_advent and timezone.localtime().date() < christmas:
         data['SEASONAL_COLOR'] = 'bg-purple'
-    elif datetime.now().date() >= christmas:
+    elif timezone.localtime().date() >= christmas:
         data['SEASONAL_COLOR'] = 'bg-gold'
     return data
