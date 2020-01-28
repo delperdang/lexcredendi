d4f5996b975fa54b1e913e8f914bfedab8013e2a
dieselpwr <dieselpwr99@gmail.com>
2020-01-28 15:45:56 -0600

    hopefully adjusted things appropriately to add new records to the calendar

--- litcal/views.py
+++ litcal/views.py
@@ -1,7 +1,11 @@
+from datetime import date
 from django.shortcuts import render
 from django.utils import timezone
 import requests
 import dateutil.parser as dparser
+from dateutil.easter import easter
+from dateutil.relativedelta import relativedelta as rd
+from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 from dateutil import tz
 from bs4 import BeautifulSoup
 
@@ -11,7 +15,7 @@ APP_FULL_NAME = 'Liturgical Calendar'
 ICON_FILENAME = 'cathedral.svg'
 
 
-class USCCB(object):
+class USCCBCalendar(object):
     '''
     Captures the latest liturgical calendar entry from the usccb readings
     '''
@@ -63,14 +67,96 @@ class USCCB(object):
         return litcal_record
 
 
+class Mysteries(object):
+    '''
+    Determines the appropriate mysteries of the Rosary for the given day and season
+    '''
+
+    # Rosary Mysteries constants
+    JOYFUL_MYSTERIES = {'title': "Today's Mysteries of the Rosary", 'text': '<a href="/doctrine/details/JOYFUL_MYSTERIES">Joyful Mysteries</a>'}
+    SORROWFUL_MYSTERIES = {'title': "Today's Mysteries of the Rosary", 'text': '<a href="/doctrine/details/SORROWFUL_MYSTERIES">Sorrowful Mysteries</a>'}
+    GLORIOUS_MYSTERIES = {'title': "Today's Mysteries of the Rosary", 'text': '<a href="/doctrine/details/GLORIOUS_MYSTERIES">Glorious Mysteries</a>'}
+    LUMINOUS_MYSTERIES = {'title': "Today's Mysteries of the Rosary", 'text': '<a href="/doctrine/details/LUMINOUS_MYSTERIES">Luminous Mysteries</a>'}
+
+    def _get_season(self, local_now):
+        '''
+        returns the current liturgical season using local time
+        '''
+        baptism_of_our_lord = date(local_now.year, 1, 6) + rd(days=1, weekday=SU(+1))
+        ash_wednesday = easter(local_now.year) - rd(days=46)
+        maundy_thursday = easter(local_now.year) - rd(days=3)
+        pentecost = easter(local_now.year) + rd(days=49)
+        first_sunday_of_advent = date(local_now.year, 12, 25) - rd(days=1, weekday=SU(-4))
+        christmas = date(local_now.year, 12, 25)
+        season = 'ORDINARY'
+        if local_now.date() < baptism_of_our_lord:
+            season = 'CHRISTMAS'
+        elif local_now.date() >= baptism_of_our_lord and local_now.date() < ash_wednesday:
+            season = 'ORDINARY'
+        elif local_now.date() >= ash_wednesday and local_now.date() < maundy_thursday:
+            season = 'LENT'
+        elif local_now.date() >= maundy_thursday and local_now.date() < easter(local_now.year):
+            season = 'TRIDUUM'
+        elif local_now.date() >= easter(local_now.year) and local_now.date() < pentecost:
+            season = 'EASTER'
+        elif local_now.date() >= pentecost and local_now.date() < first_sunday_of_advent:
+            season = 'ORDINARY'
+        elif local_now.date() >= first_sunday_of_advent and local_now.date() < christmas:
+            season = 'ADVENT'
+        elif local_now.date() >= christmas:
+            season = 'CHRISTMAS'
+        return season
+
+    def _get_upper_day_of_week(self, local_now):
+        '''
+        returns the current day of week spelled out and uppercased
+        '''
+        day_of_week = local_now.date().strftime("%A").upper()
+        return day_of_week
+
+    def get_record(self, localtime):
+        '''
+        returns a context json of the current liturgical date
+        '''
+        season_string = self._get_season(localtime)
+        dow_string = self._get_upper_day_of_week(localtime)
+        rosary_record = {}
+        if dow_string == 'SUNDAY' and season_string == 'ADVENT':
+            rosary_record = self.JOYFUL_MYSTERIES
+        elif dow_string == 'SUNDAY' and season_string == 'LENT':
+            rosary_record = self.SORROWFUL_MYSTERIES
+        elif dow_string == 'SUNDAY' and season_string == 'TRIDUUM':
+            rosary_record = self.SORROWFUL_MYSTERIES
+        elif dow_string == 'SUNDAY':
+            rosary_record = self.GLORIOUS_MYSTERIES
+        elif dow_string == 'MONDAY':
+            rosary_record = self.JOYFUL_MYSTERIES
+        elif dow_string == 'TUESDAY':
+            rosary_record = self.SORROWFUL_MYSTERIES
+        elif dow_string == 'WEDNESDAY':
+            rosary_record = self.GLORIOUS_MYSTERIES
+        elif dow_string == 'THURSDAY':
+            rosary_record = self.LUMINOUS_MYSTERIES
+        elif dow_string == 'FRIDAY':
+            rosary_record = self.SORROWFUL_MYSTERIES
+        elif dow_string == 'SATURDAY':
+            rosary_record = self.JOYFUL_MYSTERIES
+        return rosary_record
+
+
 def home(request):
-    usccb = USCCB()
-    record = usccb.get_record(timezone.localtime())
+    records = []
+    usccb_calendar = USCCBCalendar()
+    usccb_calendar_record = usccb_calendar.get_record(timezone.localtime())
+    records.append(usccb_calendar_record)
+    mysteries = Mysteries()
+    mysteries_record = mysteries.get_record(timezone.localtime())
+    records.append(mysteries_record)
 
     context = {
         'app_full_name': APP_FULL_NAME,
         'icon_filename': ICON_FILENAME,
-        'record': record
+        'records': records
     }
 
-    return render(request, 'home/details.html', context)
+    return render(request, 'litcal/home.html', context)
