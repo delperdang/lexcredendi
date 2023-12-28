af805c6a844d211d7ca71212a9f67c6d6509edce
delperdang <steve.delperdang@gmail.com>
2023-12-28 08:57:19 -0500

    remove import export function from art app

--- art/admin.py
+++ art/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from art.resource import RecordResource
 from art.models import Record
-
-class RecordAdmin(ImportExportModelAdmin):
-    resource_class = RecordResource
 
 # Register your models here.
 
-admin.site.register(Record, RecordAdmin)
+admin.site.register(Record)
--- art/resource.py
+++
@@ -1,7 +0,0 @@
-from import_export import resources 
-from art.models import Record
-
-class RecordResource(resources.ModelResource):
-
-    class Meta:
-        model = Record
