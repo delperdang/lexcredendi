fcf9647a49d3f0d09befd28ba2770247f5d6554f
delperdang <steve.delperdang@gmail.com>
2023-12-30 20:22:55 -0500

    removed piety app because of low use

--- piety/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from piety.resource import RecordResource
-from piety.models import Record
-
-class RecordAdmin(ImportExportModelAdmin):
-    resource_class = RecordResource
-
-admin.site.register(Record, RecordAdmin)
--- piety/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PietyConfig(AppConfig):
-    name = 'piety'
