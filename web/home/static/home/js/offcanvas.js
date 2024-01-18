5e22bb8a98cd2d1ecce756e07c138b63f614f649
delperdang <steve.delperdang@gmail.com>
2024-01-18 16:13:20 -0500

    experiment with non-https on rfc 1918 and loopback

--- web/home/static/home/js/offcanvas.js
+++ web/home/static/home/js/offcanvas.js
@@ -10,4 +10,26 @@ $(function () {
   //     location.protocol = "https:";
   //   };
 
+  function isLocalAddress(hostname) {
+    const rfc1918Ranges = [
+      /^10\./,
+      /^172\.(1[6-9]|2\d|3[0-1])\./,
+      /^192\.168\./,
+    ];
+    const loopbackAddresses = ["127.0.0.1", "::1"];
+  
+    return (
+      rfc1918Ranges.some((range) => range.test(hostname)) ||
+      loopbackAddresses.includes(hostname) ||
+      hostname.toLowerCase() === "localhost"
+    );
+  }
+  
+  const hostname = location.hostname;
+  const protocol = location.protocol;
+  
+  if (!isLocalAddress(hostname) && protocol !== "https:") {
+    location.protocol = "https:";
+  }
+  
 })
