3f1dc0a947ea031ce555d93be3a06fb3e2811a94
delperdang <steve.delperdang@gmail.com>
2024-02-20 02:47:35 +0000

    preparing to run without ssl

--- web/gunicorn_config.py
+++ web/gunicorn_config.py
@@ -5,6 +5,6 @@ workers = 2
 worker_connections = 1000
 threads = 4
 
-# comment these lines when encryption is not yet active
-certfile = "/etc/letsencrypt/live/lexcredendi.com/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/lexcredendi.com/privkey.pem"
+# TODO: comment these lines when encryption is not yet active
+# certfile = "/etc/letsencrypt/live/lexcredendi.com/fullchain.pem"
+# keyfile = "/etc/letsencrypt/live/lexcredendi.com/privkey.pem"
