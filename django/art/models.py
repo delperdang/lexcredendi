10f4686e4d3d4110dab5e2f089781c0907ed16d1
delperdang <steve.delperdang@gmail.com>
2024-03-03 21:36:27 +0000

    Fixed error with image titles in media uploads

--- django/art/models.py
+++ django/art/models.py
@@ -5,6 +5,6 @@ class Record(models.Model):
     image = models.ImageField(_('Image'), upload_to="art/img", max_length=100)
     album = models.CharField(_('Album'), max_length=255)
     def __str__(self):
-        return ('{}').format(self.title)
+        return ('{}').format(self.image)
     class Meta:
         verbose_name_plural = "Records"
