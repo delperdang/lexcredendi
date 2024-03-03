0973fd08fd1e9092667ca95b6281544b956a8aae
delperdang <steve.delperdang@gmail.com>
2024-03-03 20:53:40 +0000

    removed unnecessary validators from art

--- django/art/models.py
+++ django/art/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 from django.utils.translation import gettext as _
-from django.core.validators import RegexValidator
 
 class Record(models.Model):
     image = models.ImageField(_('Image'), upload_to="art/img", max_length=100)
-    album = models.CharField(_('Album'), max_length=255, validators=[RegexValidator(r'^[A-Z_]+$')])
+    album = models.CharField(_('Album'), max_length=255)
     def __str__(self):
         return ('{}').format(self.title)
     class Meta:
